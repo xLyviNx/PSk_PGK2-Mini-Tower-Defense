cd76ebefcd22cc1bb0d71c2170346bfb99bf3cf9
LyviN <LyviNSFM@gmail.com>
2024-05-10 16:48:37 +0200

    .

--- Engine/Core/Mesh.cs
+++ Engine/Core/Mesh.cs
@@ -1,165 +1,72 @@
-﻿using Assimp.Configs;
-using Assimp;
+﻿
 using System.Runtime.InteropServices;
 using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
+using PGK2.Engine.Components.Base;
 
 namespace PGK2.Engine.Core
 {
-	public class Mesh
-	{
-		private static Dictionary<string, Mesh> LoadedMeshes = new();
-		private Dictionary<int, List<MeshVertex>> MaterialParts = new Dictionary<int, List<MeshVertex>>();
-
-		// Bufor OpenGL dla wierzchołków
-		private int VertexBufferObject;
-		private int VertexArrayObject;
-
-		public List<Material> LoadedMaterials { get; private set; }
-		public List<MeshVertex> Vertices { get; private set; }
-
-		public Mesh()
-		{
-			Vertices = new List<MeshVertex>();
-			VertexBufferObject = 0;
-			LoadedMaterials = new();
-		}
-		public float[] ExtractVerts()
-		{
-			List<float> vertsList = new();
-			foreach (var v in Vertices)
-			{
-				vertsList.Add(v.Position.X);
-				vertsList.Add(v.Position.Y);
-				vertsList.Add(v.Position.Z);
-			};
-			return vertsList.ToArray();
-		}
-		public void Unload()
-		{
-			if (VertexBufferObject != 0)
-			{
-				GL.DeleteBuffer(VertexBufferObject);
-				VertexBufferObject = 0;
-			}
-
-			if (LoadedMaterials != null)
-			{
-				foreach (var material in LoadedMaterials)
-				{
-					//material?.Texture2D?.Dispose();
-				}
-			}
-		}
-
-		public static Mesh LoadFromFile(string filePath)
-		{
-			if (LoadedMeshes.ContainsKey(filePath))
-				return LoadedMeshes[filePath];
-
-			AssimpContext context = new AssimpContext();
-			context.SetConfig(new NormalSmoothingAngleConfig(66.0f));
-
-			Scene scene = context.ImportFile(filePath, PostProcessSteps.Triangulate | PostProcessSteps.FlipUVs);
-
-			if (scene == null || scene.Meshes.Count == 0)
-				throw new Exception("Failed to load mesh.");
-
-			Mesh loadedMesh = new Mesh();
-
-			foreach (var mesh in scene.Meshes)
-			{
-
-				// Przetwórz informacje o meshu, takie jak wierzchołki, indeksy itd.
-				for (int i = 0; i < mesh.Vertices.Count; i++)
-				{
-
-					MeshVertex vertex = new MeshVertex(
-						new Vector3(mesh.Vertices[i].X, mesh.Vertices[i].Y, mesh.Vertices[i].Z),
-						new Vector3(mesh.Normals[i].X, mesh.Normals[i].Y, mesh.Normals[i].Z),
-						mesh.HasTextureCoords(0) ? new Vector2(mesh.TextureCoordinateChannels[0][i].X, mesh.TextureCoordinateChannels[0][i].Y) : Vector2.Zero
-					);
-
-					loadedMesh.Vertices.Add(vertex);
-
-
-					// Podziel wierzchołki na części w zależności od materiału
-					int materialIndex = mesh.MaterialIndex;
-					if (!loadedMesh.MaterialParts.ContainsKey(materialIndex))
-						loadedMesh.MaterialParts[materialIndex] = new List<MeshVertex>();
-
-					loadedMesh.MaterialParts[materialIndex].Add(vertex);
-				}
-			}
-
-			// Przetwórz materiały z sceny, jeśli są dostępne.
-			if (scene.Materials.Count > 0)
-			{
-				loadedMesh.LoadedMaterials = new();
-
-				foreach (var material in scene.Materials)
-				{
-					loadedMesh.LoadedMaterials.Add(new Material(material));
-
-				}
-			}
-
-			// Utwórz bufor OpenGL dla wierzchołków
-			loadedMesh.VertexArrayObject = GL.GenVertexArray();
-			loadedMesh.VertexBufferObject = GL.GenBuffer();
-			GL.BindBuffer(BufferTarget.ArrayBuffer, loadedMesh.VertexBufferObject);
-			float[] verts = loadedMesh.ExtractVerts();
-			GL.BufferData(BufferTarget.ArrayBuffer, verts.Length * sizeof(float), verts, BufferUsageHint.StaticDraw);
-
-			GL.BindVertexArray(loadedMesh.VertexArrayObject);
-			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
-			GL.EnableVertexArrayAttrib(loadedMesh.VertexArrayObject, 0);
-
-
-			LoadedMeshes[filePath] = loadedMesh;
-			GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-			GL.BindVertexArray(0);
-			return loadedMesh;
-		}
-		
-		public void Render(int materialIndex)
-		{
-			if (!MaterialParts.ContainsKey(materialIndex))
-				return;
-			GL.BindVertexArray(VertexArrayObject);
-			/*// Ustawienia atrybutów dla wierzchołków (position, normal, texCoord)
-			int positionLocation = 0; // Przyjmujemy, że atrybuty w shaderze są ustawione na lokalizacjach 0, 1 i 2
-			GL.EnableVertexAttribArray(positionLocation);
-			GL.VertexAttribPointer(positionLocation, 3, VertexAttribPointerType.Float, false, Marshal.SizeOf<MeshVertex>(), 0);
-
-			int normalLocation = 1;
-			GL.EnableVertexAttribArray(normalLocation);
-			GL.VertexAttribPointer(normalLocation, 3, VertexAttribPointerType.Float, false, Marshal.SizeOf<MeshVertex>(), Vector3.SizeInBytes);
-
-			/*int texCoordLocation = 2;
-			*GL.EnableVertexAttribArray(texCoordLocation);
-			GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, Marshal.SizeOf<MeshVertex>(), Vector3.SizeInBytes * 2);
-			*/
-			// Renderowanie
-			GL.DrawArrays(OpenTK.Graphics.OpenGL4.PrimitiveType.Triangles, 0, Vertices.Count);
-
-			// Wyłączenie atrybutów po renderowaniu
-			//GL.DisableVertexAttribArray(positionLocation);
-			//GL.DisableVertexAttribArray(normalLocation);
-			//GL.DisableVertexAttribArray(texCoordLocation);
-		}
-	}
 	public struct MeshVertex
 	{
-		public Vector3 Position { get; }
-		public Vector3 Normal { get; }
-		public Vector2 TexCoord { get; }
+		public Vector3 Position { get; set;  }
+		public Vector3 Normal { get; set; }
+		public Vector2 TexCoords { get; set; }
 
 		public MeshVertex(Vector3 position, Vector3 normal, Vector2 texCoord)
 		{
 			Position = position;
 			Normal = normal;
-			TexCoord = texCoord;
+			TexCoords = texCoord;
 		}
 	}
+	public struct Texture
+	{
+		uint id;
+		string type;
+	};
+	public class Mesh
+	{
+		private uint VAO, VBO, EBO;
+		public List<MeshVertex> vertices;
+		public List<uint> indices;
+		public List<Texture> textures;
+		public Mesh(List<MeshVertex> vertices, List<uint> indices, List<Texture> textures)
+		{
+			this.vertices = vertices;
+			this.indices = indices;
+			this.textures = textures;
+			setupMesh();
+		}
+		public void Draw(ref CameraComponent camera)
+		{
+			GL.BindVertexArray(VAO);
+			GL.DrawElements(BeginMode.Triangles, indices.Count, DrawElementsType.UnsignedInt, 0);
+			GL.BindVertexArray(0);
+		}
+
+		void setupMesh()
+		{
+			GL.GenVertexArrays(1, out VAO);
+			GL.GenBuffers(1, out VBO);
+			GL.GenBuffers(1, out EBO);
+
+			GL.BindVertexArray(VAO);
+			GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+			GL.BufferData(BufferTarget.ArrayBuffer, vertices.Count * Marshal.SizeOf<MeshVertex>(), vertices.ToArray(), BufferUsageHint.StaticDraw);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, EBO);
+			GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Count * sizeof(uint), indices.ToArray(), BufferUsageHint.StaticDraw);
+
+			GL.EnableVertexAttribArray(0);
+			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, Marshal.SizeOf<MeshVertex>(), 0);
+
+			GL.EnableVertexAttribArray(1);
+			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, Marshal.SizeOf<MeshVertex>(), Marshal.OffsetOf<MeshVertex>("Normal"));
+
+			GL.EnableVertexAttribArray(2);
+			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, Marshal.SizeOf<MeshVertex>(), Marshal.OffsetOf<MeshVertex>("TexCoords"));
+
+			GL.BindVertexArray(0);
+		}
+	};
 }
---
+++ Engine/Core/Model.cs
@@ -0,0 +1,120 @@
+﻿using Assimp;
+using PGK2.Engine.Components.Base;
+using Assimp.Configs;
+using OpenTK.Mathematics;
+using Assimp.Unmanaged;
+using System.Numerics;
+
+namespace PGK2.Engine.Core
+{
+	public class Model
+	{
+
+		public Model(string path)
+		{
+			loadModel(path);
+		}
+		public void Draw(ref CameraComponent camera)
+		{
+			foreach(var mesh in meshes)
+				mesh.Draw(ref camera);
+		}
+
+		private List<Mesh> meshes;
+		private string directory;
+
+		private void loadModel(string path)
+		{
+			AssimpContext context = new AssimpContext();
+			context.SetConfig(new NormalSmoothingAngleConfig(66.0f));
+
+			Assimp.Scene scene = context.ImportFile(path, PostProcessSteps.Triangulate | PostProcessSteps.FlipUVs);
+
+			if (scene == null || scene.Meshes.Count == 0)
+				throw new Exception("Failed to load mesh.");
+
+			directory = path.Substring(0, path.LastIndexOf('/'));
+			processNode(scene.RootNode, scene);
+		}
+		private void processNode(Assimp.Node node, in Assimp.Scene scene)
+		{
+			for (int i = 0; i < node.MeshCount; i++)
+			{
+				Assimp.Mesh mesh = scene.Meshes[node.MeshIndices[i]];
+				meshes.Add(processMesh(mesh, scene));
+			}
+			// then do the same for each of its children
+			for (int i = 0; i < node.ChildCount; i++)
+			{
+				processNode(node.Children[i], scene);
+			}
+		}
+
+		private Mesh processMesh(Assimp.Mesh mesh, in Assimp.Scene scene)
+		{
+			List<MeshVertex> vertices = new();
+			List<uint> indices=new();
+			List<Texture> textures=new();
+
+			for (int i = 0; i < mesh.VertexCount; i++)
+			{
+				MeshVertex vertex = new();
+
+				Vector3 vector;
+				vector.X = mesh.Vertices[i].X;
+				vector.Y = mesh.Vertices[i].Y;
+				vector.Z = mesh.Vertices[i].Z;
+				vertex.Position = vector;
+
+				vertices.Add(vertex);
+
+				vector.X = mesh.Normals[i].X;
+				vector.Y = mesh.Normals[i].Y;
+				vector.Z = mesh.Normals[i].Z;
+				vertex.Normal = vector;
+
+				if (mesh.HasTextureCoords(0))
+				{
+					Vector2 vec;
+					vec.X = mesh.TextureCoordinateChannels[0][i].X;
+					vec.Y = mesh.TextureCoordinateChannels[0][i].Y;
+					vertex.TexCoords = vec;
+				}
+				else
+					vertex.TexCoords = new Vector2(0.0f, 0.0f);
+			}
+			for (int i = 0; i < mesh.FaceCount; i++)
+			{
+				Assimp.Face face = mesh.Faces[i];
+				for (int j = 0; j < face.IndexCount; j++)
+					indices.Add((uint)face.Indices[j]);
+			}
+			// process material
+			if (mesh.MaterialIndex >= 0)
+			{
+				Assimp.Material material = scene.Materials[mesh.MaterialIndex];
+				List<Texture> diffuseMaps = LoadMaterialTextures(material,
+													TextureType.Diffuse, "texture_diffuse");
+				textures.AddRange(diffuseMaps);
+				List<Texture> specularMaps = LoadMaterialTextures(material,
+													TextureType.Specular, "texture_specular");
+				textures.AddRange(specularMaps);
+			}
+			return new Mesh(vertices, indices, textures);
+		}
+		List<Texture> LoadMaterialTextures(Assimp.Material mat, TextureType type, string typeName)
+		{
+			List<Texture> textures = new List<Texture>();
+			for (int i = 0; i < mat.GetMaterialTextureCount(type); i++)
+			{
+				TextureSlot textureSlot;
+				mat.GetMaterialTexture(type, i, out textureSlot);
+				Texture texture = new Texture();
+				texture.Id = TextureFromFile(textureSlot.FilePath, directory);
+				texture.Type = typeName;
+				texture.Path = textureSlot.FilePath;
+				textures.Add(texture);
+			}
+			return textures;
+		}
+	}
