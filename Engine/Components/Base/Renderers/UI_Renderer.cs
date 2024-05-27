ee6a5d09ebc864aefdd4dbbfec08523422f105eb
LyviN <LyviNSFM@gmail.com>
2024-05-27 08:42:32 +0200

    Click, Hover, Exit events for UI Text

--- Engine/Components/Base/Renderers/UI_Renderer.cs
+++ Engine/Components/Base/Renderers/UI_Renderer.cs
@@ -1,5 +1,7 @@
-﻿using PGK2.Engine.Core;
+﻿using ImGuiNET;
+using PGK2.Engine.Core;
 using PGK2.Engine.SceneSystem;
+using System;
 using System.Numerics;
 using System.Text.Json.Serialization;
 
@@ -10,16 +12,40 @@ namespace PGK2.Engine.Components.Base
 		[JsonIgnore]
 		public Vector2 UI_Position => new Vector2(transform.Position.X, transform.Position.Y);
 		public Vector4 Color = new Vector4(1, 1, 1, 1);
+
+		public event Action OnClick = delegate { };
+		public event Action OnHover = delegate { };
+		public event Action OnExit = delegate { };
+
+		public bool wasHovered = false;
+
 		public UI_Renderer()
 		{
 			OnSceneTransfer += SceneTransfer;
 		}
+
+		public virtual void Clicked()
+		{
+			OnClick.Invoke();
+		}
+
+		public virtual void Hovered()
+		{
+			OnHover.Invoke();
+		}
+
+		public virtual void Exited()
+		{
+			OnExit.Invoke();
+		}
+
 		public override void OnDestroy()
 		{
 			if (OnSceneTransfer != null)
 				OnSceneTransfer -= SceneTransfer;
 			base.OnDestroy();
 		}
+
 		private void SceneTransfer(Scene? oldscene)
 		{
 			if (oldscene != null)
@@ -31,13 +57,32 @@ namespace PGK2.Engine.Components.Base
 
 		internal virtual void Draw()
 		{
-
 		}
 
 		internal void CallDraw()
 		{
 			if (EnabledInHierarchy)
 				Draw();
+		}
+
+		protected void HandleHoverState()
+		{
+			if (ImGui.IsItemHovered())
+			{
+				if (!wasHovered)
+				{
+					Hovered();
+					wasHovered = true;
+				}
+			}
+			else
+			{
+				if (wasHovered)
+				{
+					Exited();
+					wasHovered = false;
+				}
+			}
 		}
 	}
 }
--- Engine/Components/Base/Renderers/UI_Text.cs
+++ Engine/Components/Base/Renderers/UI_Text.cs
@@ -1,4 +1,5 @@
 ﻿using ImGuiNET;
+using System;
 using System.Numerics;
 
 namespace PGK2.Engine.Components.Base.Renderers
@@ -8,14 +9,38 @@ namespace PGK2.Engine.Components.Base.Renderers
 		public string Text = "New Text";
 		public float FontSize = 1;
 
+		public override void Update()
+		{
+			base.Update();
+			if (wasHovered)
+				Color = new(1, 0, 0, 1);
+			else
+				Color = new(0, 1, 1, 1);
+		}
 		internal override void Draw()
 		{
 			ImGui.SetNextWindowPos(UI_Position, ImGuiCond.Always);
 			ImGui.PushStyleColor(ImGuiCol.Text, Color);
-			ImGui.SetNextWindowBgAlpha(0.0f); // Transparent background
+			ImGui.SetNextWindowBgAlpha(0.0f);
 			ImGui.Begin("Text", ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoBackground);
-			ImGui.SetWindowFontScale(FontSize); 
+
+			ImGui.SetWindowFontScale(FontSize);
+
+			Vector2 textSize = ImGui.CalcTextSize(Text);
+
+			ImGui.InvisibleButton("##invisible_button", textSize);
+
+			if (ImGui.IsItemClicked())
+			{
+				Console.WriteLine("CLICK");
+				Clicked();
+			}
+
+			HandleHoverState();
+
+			ImGui.SetCursorPos(ImGui.GetCursorPos() - new Vector2(0, textSize.Y)); // Reset cursor position
 			ImGui.Text(Text);
+
 			ImGui.End();
 			ImGui.PopStyleColor();
 		}
